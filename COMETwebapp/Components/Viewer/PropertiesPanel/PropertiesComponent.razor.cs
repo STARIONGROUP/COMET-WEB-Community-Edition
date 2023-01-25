2c4c5725b67e45e805d0f0de8a09180c24dbcebc
jaimeatrhea <j.bernar@rheagroup.com>
2023-01-25 11:29:42 +0100

    Fix bug on the PopUp window always showing

--- COMETwebapp/Components/Viewer/PropertiesPanel/PropertiesComponent.razor.cs
+++ COMETwebapp/Components/Viewer/PropertiesPanel/PropertiesComponent.razor.cs
@@ -170,7 +170,6 @@ namespace COMETwebapp.Components.Viewer.PropertiesPanel
                     this.SessionService.UpdateThings(this.SessionService.DefaultIteration, new List<Thing>() { clonedParameterValueSet });
                 }
             }
-            this.SelectionMediator.SceneObjectHasChanges = false;
         }
 
         /// <summary>
