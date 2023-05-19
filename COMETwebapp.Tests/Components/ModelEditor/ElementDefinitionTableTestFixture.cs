14969299a65cadb19e0bb37f210274545ab1d585
Nabil Abbar <n.abbar@rheagroup.com>
2023-05-19 15:35:15 +0200

    Code coverage

---
+++ COMETwebapp.Tests/Components/ModelEditor/ElementDefinitionTableTestFixture.cs
@@ -0,0 +1,89 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="ElementDefinitionTableTestFixture.cs" company="RHEA System S.A.">
+//     Copyright (c) 2023 RHEA System S.A.
+// 
+//     Authors: Sam Gerené, Alex Vorobiev, Alexander van Delft, Jaime Bernar, Théate Antoine, Nabil Abbar
+// 
+//     This file is part of COMET WEB Community Edition
+//     The COMET WEB Community Edition is the RHEA Web Application implementation of ECSS-E-TM-10-25 Annex A and Annex C.
+// 
+//     The COMET WEB Community Edition is free software; you can redistribute it and/or
+//     modify it under the terms of the GNU Affero General Public
+//     License as published by the Free Software Foundation; either
+//     version 3 of the License, or (at your option) any later version.
+// 
+//     The COMET WEB Community Edition is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Affero General Public License for more details.
+// 
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace COMETwebapp.Tests.Components.ModelEditor
+{
+    using System.Collections.ObjectModel;
+
+    using Bunit;
+
+    using COMET.Web.Common.Services.SessionManagement;
+    using COMET.Web.Common.Test.Helpers;
+
+    using COMETwebapp.Components.ModelEditor;
+    using COMETwebapp.ViewModels.Components.ModelEditor;
+    using COMETwebapp.ViewModels.Components.SystemRepresentation.Rows;
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    using TestContext = Bunit.TestContext;
+
+    [TestFixture]
+    public class ElementDefinitionTableTestFixture
+    {
+        private TestContext context;
+        private IRenderedComponent<ElementDefinitionTable> renderedComponent;
+        private ElementDefinitionTable table;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.context = new TestContext();
+            this.context.ConfigureDevExpressBlazor();
+            this.context.Services.AddSingleton<ISessionService, SessionService>();
+
+            var elementDefinitionTableViewModel = new Mock<IElementDefinitionTableViewModel>();
+            elementDefinitionTableViewModel.Setup(x => x.RowsTarget).Returns(new ObservableCollection<ElementDefinitionRowViewModel>());
+            elementDefinitionTableViewModel.Setup(x => x.RowsSource).Returns(new ObservableCollection<ElementDefinitionRowViewModel>());
+
+            this.context.Services.AddSingleton(elementDefinitionTableViewModel.Object);
+
+            this.renderedComponent = this.context.RenderComponent<ElementDefinitionTable>();
+
+            this.table = this.renderedComponent.Instance;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.context.CleanContext();
+        }
+
+        [Test]
+        public void VerifyComponent()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(this.renderedComponent, Is.Not.Null);
+                Assert.That(this.table, Is.Not.Null);
+                Assert.That(this.table.ViewModel, Is.Not.Null);
+                Assert.That(this.table.JS, Is.Not.Null);
+            });
+        }
+    }
+}
---
+++ COMETwebapp.Tests/ViewModels/Components/ModelEditor/ElementDefinitionTableViewModelTestFixture.cs
@@ -0,0 +1,105 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="ElementDefinitionTableViewModelTestFixture.cs" company="RHEA System S.A.">
+//     Copyright (c) 2023 RHEA System S.A.
+// 
+//     Authors: Sam Gerené, Alex Vorobiev, Alexander van Delft, Jaime Bernar, Théate Antoine, Nabil Abbar
+// 
+//     This file is part of COMET WEB Community Edition
+//     The COMET WEB Community Edition is the RHEA Web Application implementation of ECSS-E-TM-10-25 Annex A and Annex C.
+// 
+//     The COMET WEB Community Edition is free software; you can redistribute it and/or
+//     modify it under the terms of the GNU Affero General Public
+//     License as published by the Free Software Foundation; either
+//     version 3 of the License, or (at your option) any later version.
+// 
+//     The COMET WEB Community Edition is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Affero General Public License for more details.
+// 
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace COMETwebapp.Tests.ViewModels.Components.ModelEdior
+{
+    using CDP4Common.EngineeringModelData;
+
+    using CDP4Dal;
+
+    using COMET.Web.Common.Services.SessionManagement;
+
+    using COMETwebapp.ViewModels.Components.ModelEditor;
+
+    using DynamicData;
+
+    using Moq;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ElementDefinitionTableViewModelTestFixture
+    {
+        private ElementDefinitionTableViewModel viewModel;
+        private Mock<ISessionService> sessionService;
+        private Iteration iteration;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.sessionService = new Mock<ISessionService>();
+            var session = new Mock<ISession>();
+            this.sessionService.Setup(x => x.Session).Returns(session.Object);
+
+            var topElement = new ElementDefinition()
+            {
+                Iid = Guid.NewGuid(),
+                Name = "Container",
+            };
+
+            var elementDefinition = new ElementDefinition()
+            {
+                Iid = Guid.NewGuid(),
+                Name = "Box",
+            };
+
+            var usage1 = new ElementUsage()
+            {
+                Name = "Box1",
+                Iid = Guid.NewGuid(),
+                ElementDefinition = elementDefinition
+            };
+
+            topElement.ContainedElement.Add(usage1);
+
+            this.iteration = new Iteration()
+            {
+                Iid = Guid.NewGuid(),
+                Element = { topElement, elementDefinition }
+            };
+
+            var iterations = new SourceList<Iteration>();
+            iterations.Add(this.iteration);
+
+            this.sessionService.Setup(x => x.OpenIterations).Returns(iterations);
+            this.viewModel = new ElementDefinitionTableViewModel(this.sessionService.Object);
+        }
+
+        [Test]
+        public void VerifyInitializeViewModel()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(this.viewModel.Elements.Count, Is.EqualTo(3));
+                Assert.That(this.viewModel.RowsSource, Has.Count.EqualTo(3));
+                Assert.That(this.viewModel.RowsTarget, Has.Count.EqualTo(3));
+                Assert.That(this.viewModel.RowsSource[0].ElementDefinitionName, Is.EqualTo("Container"));
+                Assert.That(this.viewModel.RowsSource[0].ElementUsageName, Is.Null);
+                Assert.That(this.viewModel.RowsSource[1].ElementDefinitionName, Is.EqualTo("Container"));
+                Assert.That(this.viewModel.RowsSource[1].ElementUsageName, Is.EqualTo("Box1"));
+                Assert.That(this.viewModel.RowsSource[0].ElementBase, Is.EqualTo(this.iteration.Element.FirstOrDefault()));
+            });
+        }
+    }
+}
