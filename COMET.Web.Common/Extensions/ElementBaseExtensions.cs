8fcde81eee5043bff0e0735a56d2f77d6b714538
Jaime Bernar <112856984+jaimeatrhea@users.noreply.github.com>
2023-07-07 10:54:03 +0200

    Upgrade devExpress. Add new Extension Methods (#409)

---
+++ COMET.Web.Common/Extensions/ElementBaseExtensions.cs
@@ -0,0 +1,77 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="ElementBaseExtensions.cs" company="RHEA System S.A.">
+//    Copyright (c) 2023 RHEA System S.A.
+// 
+//    Authors: Sam Gerené, Alex Vorobiev, Alexander van Delft, Jaime Bernar, Théate Antoine, Nabil Abbar
+// 
+//    This file is part of COMET WEB Community Edition
+//    The COMET WEB Community Edition is the RHEA Web Application implementation of ECSS-E-TM-10-25
+//    Annex A and Annex C.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// 
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace COMET.Web.Common.Extensions
+{
+    using CDP4Common.CommonData;
+    using CDP4Common.EngineeringModelData;
+
+    /// <summary>
+    /// Extension methods for <see cref="ElementBase"/>
+    /// </summary>
+    public static class ElementBaseExtensions
+    {
+        /// <summary>
+        /// Queries the <see cref="ParameterOrOverrideBase"/> of the <paramref name="elementBase"/> and all the contained elements. Goes full deep in the hierarchy
+        /// </summary>
+        /// <param name="elementBase">the <see cref="ElementBase"/> where to start the search</param>
+        /// <returns>a collection of <see cref="ParameterOrOverrideBase"/></returns>
+        public static IEnumerable<ParameterOrOverrideBase> QueryParameterAndOverrideBasesDeep(this ElementBase elementBase)
+        {
+            var parameters = new List<ParameterOrOverrideBase>();
+            QueryParameterAndOverrideBasesDeepHelper(elementBase, ref parameters);
+            return parameters;
+        }
+
+        /// <summary>
+        /// Helper method for the <see cref="QueryParameterAndOverrideBasesDeep(ElementBase)"/>
+        /// </summary>
+        /// <param name="thing">the current evaluated <see cref="Thing"/></param>
+        /// <param name="parameters">the current found <see cref="ParameterOrOverrideBase"/></param>
+        private static void QueryParameterAndOverrideBasesDeepHelper(Thing thing, ref List<ParameterOrOverrideBase> parameters)
+        {
+            if (thing is ElementDefinition elementDefinition)
+            {
+                foreach (var elementUsage in elementDefinition.ContainedElement)
+                {
+                    QueryParameterAndOverrideBasesDeepHelper(elementUsage, ref parameters);
+                }
+
+                foreach (var parameter in elementDefinition.Parameter)
+                {
+                    QueryParameterAndOverrideBasesDeepHelper(parameter, ref parameters);
+                }
+            }
+            else if (thing is ElementUsage elementUsage)
+            {
+                QueryParameterAndOverrideBasesDeepHelper(elementUsage.ElementDefinition, ref parameters);
+            }
+            else if (thing is ParameterOrOverrideBase parameterOrOverride)
+            {
+                parameters.Add(parameterOrOverride);
+            }
+        }
+    }
+}
---
+++ COMET.Web.Common/Extensions/ParameterOrOverrideBaseExtensions.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="ParameterOrOverrideBaseExtensions.cs" company="RHEA System S.A.">
+//    Copyright (c) 2023 RHEA System S.A.
+// 
+//    Authors: Sam Gerené, Alex Vorobiev, Alexander van Delft, Jaime Bernar, Théate Antoine, Nabil Abbar
+// 
+//    This file is part of COMET WEB Community Edition
+//    The COMET WEB Community Edition is the RHEA Web Application implementation of ECSS-E-TM-10-25
+//    Annex A and Annex C.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// 
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace COMET.Web.Common.Extensions
+{
+    using CDP4Common.EngineeringModelData;
+
+    /// <summary>
+    /// Extension methods for <see cref="ParameterOrOverrideBase"/>
+    /// </summary>
+    public static class ParameterOrOverrideBaseExtensions
+    {
+        /// <summary>
+        /// Checks that all the parameters have a value different that the initial value "-"
+        /// </summary>
+        /// <param name="parameters">the parameters to check</param>
+        /// <returns>true if all the parameters have a value, false otherwise</returns>
+        public static bool AllParametersHaveValues(this IEnumerable<ParameterOrOverrideBase> parameters)
+        {
+            var stringValues = parameters.SelectMany(x => x.ValueSets).SelectMany(x => x.ActualValue).ToList();
+            return stringValues.All(x => x != "-");
+        }
+
+        /// <summary>
+        /// Checks that all the parameters have been modified before the <paramref name="dateTime"/>
+        /// </summary>
+        /// <param name="parameters">the parameters to check</param>
+        /// <param name="dateTime">the date time to check</param>
+        /// <returns>true if all the parameters have been modified before the date, false otherwise</returns>
+        public static bool AllParametersFilledBefore(this IEnumerable<ParameterOrOverrideBase> parameters, DateTime dateTime)
+        {
+            return parameters.SelectMany(x => x.ValueSets).Cast<ParameterValueSetBase>().All(x => x.ModifiedOn < dateTime);
+        }
+    }
+}
