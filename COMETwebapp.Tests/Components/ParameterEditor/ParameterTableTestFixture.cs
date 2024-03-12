95805809715af9aafe3bf61ccf1a4f46e9f42399
Joao Rua <joao.rua@4all-software.com>
2024-03-12 10:45:29 +0000

    now group rows are also highlighted when a parameter is publishable

--- COMETwebapp.Tests/Components/ParameterEditor/ParameterTableTestFixture.cs
+++ COMETwebapp.Tests/Components/ParameterEditor/ParameterTableTestFixture.cs
@@ -27,6 +27,10 @@ namespace COMETwebapp.Tests.Components.ParameterEditor
     using Bunit;
 
     using CDP4Common.EngineeringModelData;
+    using CDP4Common.SiteDirectoryData;
+    using CDP4Common.Types;
+
+    using CDP4Dal;
 
     using COMET.Web.Common.Services.SessionManagement;
     using COMET.Web.Common.Test.Helpers;
@@ -52,31 +56,32 @@ namespace COMETwebapp.Tests.Components.ParameterEditor
         private TestContext context;
         private IRenderedComponent<ParameterTable> renderedComponent;
         private ParameterTable table;
-        
+        private Mock<ISessionService> sessionService;
+        private ICDPMessageBus messageBus;
+        private Mock<IParameterTableViewModel> parameterTableViewModel;
+
         [SetUp]
         public void SetUp()
         {
             this.context = new TestContext();
             this.context.ConfigureDevExpressBlazor();
-            this.context.Services.AddSingleton<ISessionService, SessionService>();
+            this.sessionService = new Mock<ISessionService>();
+            this.context.Services.AddSingleton(this.sessionService.Object);
+            this.messageBus = new CDPMessageBus();
 
-            var parameterTableViewModel = new Mock<IParameterTableViewModel>();
-            parameterTableViewModel.Setup(x => x.Rows).Returns(new SourceList<ParameterBaseRowViewModel>());
+            this.parameterTableViewModel = new Mock<IParameterTableViewModel>();
 
-            var element1 = new ElementDefinition();
+            var parametersList = new SourceList<ParameterBaseRowViewModel>();
+            parametersList.Add(this.GenerateIntegerRow("n1", "group1", 1, 2));
+            parametersList.Add(this.GenerateIntegerRow("n2", "group1", 1, 1));
+            parametersList.Add(this.GenerateIntegerRow("n3", "group2", 1, 1));
+            parametersList.Add(this.GenerateIntegerRow("n4", "group2", 1, 1));
 
-            var element2 = new ElementUsage()
-            {
-                ElementDefinition = new ElementDefinition()
-            };
-
-            var elementList = new SourceList<ElementBase>();
-            elementList.Add(element1);
-            elementList.Add(element2);
+            this.parameterTableViewModel.Setup(x => x.Rows).Returns(parametersList);
 
             this.renderedComponent = this.context.RenderComponent<ParameterTable>(parameters =>
             {
-                parameters.Add(p => p.ViewModel, parameterTableViewModel.Object);
+                parameters.Add(p => p.ViewModel, this.parameterTableViewModel.Object);
             });
             
             this.table = this.renderedComponent.Instance;
@@ -85,7 +90,9 @@ namespace COMETwebapp.Tests.Components.ParameterEditor
         [TearDown]
         public void TearDown()
         {
+            this.context.Dispose();
             this.context.CleanContext();
+            this.messageBus.Dispose();
         }
 
         [Test]
@@ -97,6 +104,52 @@ namespace COMETwebapp.Tests.Components.ParameterEditor
                 Assert.That(this.table, Is.Not.Null);
                 Assert.That(this.table.ViewModel, Is.Not.Null);
             });
+
+            var grid = this.renderedComponent.FindComponent<DxGrid>();
+            var highlightedGroupRows = grid.FindAll(".font-weight-bold");
+            var expectedNumberOfGroups = this.parameterTableViewModel.Object.Rows.Items.DistinctBy(x => x.ElementBaseName).Count();
+            var expectedNumberOfDataRows = this.parameterTableViewModel.Object.Rows.Count;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(grid.Instance.GetVisibleRowCount(), Is.EqualTo(expectedNumberOfGroups));
+                Assert.That(highlightedGroupRows.Count, Is.EqualTo(1));
+            });
+            
+            grid.SetParametersAndRender(p => p.Add(x => x.AutoExpandAllGroupRows, true));
+            var highlightedRows = grid.FindAll(".font-weight-bold");
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(grid.Instance.GetVisibleRowCount(), Is.EqualTo(expectedNumberOfDataRows + expectedNumberOfGroups));
+                Assert.That(highlightedRows.Count, Is.EqualTo(2));
+            });
+        }
+
+        private ParameterBaseRowViewModel GenerateIntegerRow(string parameterShortName, string elementBaseShortName, int publishedValue, int manualValue)
+        {
+            var parameter = new Parameter()
+            {
+                Container = new ElementDefinition()
+                {
+                    ShortName = elementBaseShortName,
+                    Name = elementBaseShortName
+                },
+                ParameterType = new SimpleQuantityKind()
+                {
+                    ShortName = parameterShortName,
+                    Name = parameterShortName
+                }
+            };
+
+            var valueSet = new ParameterValueSet()
+            {
+                Published = new ValueArray<string>([publishedValue.ToString()]),
+                Manual = new ValueArray<string>([manualValue.ToString()]),
+                ValueSwitch = ParameterSwitchKind.MANUAL
+            };
+
+            return new ParameterBaseRowViewModel(this.sessionService.Object, false, parameter, valueSet, this.messageBus);
         }
     }
 }
