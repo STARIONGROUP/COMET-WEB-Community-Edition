8c1a90a0a8da82db0bc3cacc841db19788fe4a81
João Rua <140734849+joao4all@users.noreply.github.com>
2024-03-13 09:04:51 +0000

    Feat #476 IHaveReusableRows interface introduced (#530)
    * IHaveReusableRows introduced

--- COMET.Web.Common/ViewModels/Components/Applications/ApplicationBaseViewModel.cs
+++ COMET.Web.Common/ViewModels/Components/Applications/ApplicationBaseViewModel.cs
@@ -98,6 +98,42 @@ namespace COMET.Web.Common.ViewModels.Components.Applications
         }
 
         /// <summary>
+        /// Gets or sets a collection of the registered view models with reusable rows
+        /// </summary>
+        private List<IHaveReusableRows> RegisteredViewModelsWithReusableRows { get; set; } = [];
+
+        /// <summary>
+        /// Registers a collection of view models with reusable rows
+        /// </summary>
+        /// <param name="viewModels">The view models that implement <see cref="IHaveReusableRows"/></param>
+        protected void RegisterViewModelsWithReusableRows(IEnumerable<IHaveReusableRows> viewModels)
+        {
+            this.RegisteredViewModelsWithReusableRows.AddRange(viewModels.ToList());
+        }
+
+        /// <summary>
+        /// Registers viewmodels with reusable rows
+        /// </summary>
+        /// <param name="viewModels">The view models that implement <see cref="IHaveReusableRows"/></param>
+        protected void RegisterViewModelWithReusableRows(params IHaveReusableRows[] viewModels)
+        {
+            this.RegisterViewModelsWithReusableRows(viewModels.ToList());
+        }
+
+        /// <summary>
+        /// Method used to update the view model inner components that recycle rows
+        /// </summary>
+        protected void UpdateInnerComponents()
+        {
+            foreach (var viewModel in this.RegisteredViewModelsWithReusableRows)
+            {
+                viewModel.AddRows(this.AddedThings.ToList());
+                viewModel.UpdateRows(this.UpdatedThings.ToList());
+                viewModel.RemoveRows(this.DeletedThings.ToList());
+            }
+        }
+
+        /// <summary>
         /// Handles the <see cref="SessionStatus.EndUpdate" /> message received
         /// </summary>
         /// <returns>A <see cref="Task" /></returns>
---
+++ COMET.Web.Common/ViewModels/Components/Applications/IHaveReusableRows.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="IHaveReusableRows.cs" company="RHEA System S.A.">
+//     Copyright (c) 2024 RHEA System S.A.
+// 
+//     Authors: Sam Gerené, Alex Vorobiev, Alexander van Delft, Jaime Bernar, Théate Antoine, João Rua
+// 
+//     This file is part of COMET WEB Community Edition
+//     The COMET WEB Community Edition is the RHEA Web Application implementation of ECSS-E-TM-10-25 Annex A and Annex C.
+// 
+//     The COMET WEB Community Edition is free software; you can redistribute it and/or
+//     modify it under the terms of the GNU Affero General Public
+//     License as published by the Free Software Foundation; either
+//     version 3 of the License, or (at your option) any later version.
+// 
+//     The COMET WEB Community Edition is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Affero General Public License for more details.
+// 
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace COMET.Web.Common.ViewModels.Components.Applications
+{
+    using CDP4Common.CommonData;
+
+    /// <summary>
+    /// Interface to be implemented whenever a viewmodel needs to recycle its rows
+    /// </summary>
+    public interface IHaveReusableRows
+    {
+        /// <summary>
+        /// Add rows related to <see cref="Thing" /> that has been added
+        /// </summary>
+        /// <param name="addedThings">A collection of added <see cref="Thing" /></param>
+        void AddRows(IEnumerable<Thing> addedThings);
+
+        /// <summary>
+        /// Updates rows related to <see cref="Thing" /> that have been updated
+        /// </summary>
+        /// <param name="updatedThings">A collection of updated <see cref="Thing" /></param>
+        void UpdateRows(IEnumerable<Thing> updatedThings);
+
+        /// <summary>
+        /// Remove rows related to a <see cref="Thing" /> that has been deleted
+        /// </summary>
+        /// <param name="deletedThings">A collection of deleted <see cref="Thing" /></param>
+        void RemoveRows(IEnumerable<Thing> deletedThings);
+    }
+}
