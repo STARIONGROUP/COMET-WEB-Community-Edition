4dc6f31984bf9eaaa28289e4343860d5cf2fd96f
Joao Rua <joao.rua@4all-software.com>
2024-05-29 16:14:49 +0100

    person roles refactored + loading component removed for participant/person roles

--- COMETwebapp.Tests/Components/SiteDirectory/Roles/PersonRolesTableTestFixture.cs
+++ COMETwebapp.Tests/Components/SiteDirectory/Roles/PersonRolesTableTestFixture.cs
@@ -1,32 +1,29 @@
 // --------------------------------------------------------------------------------------------------------------------
-// <copyright file="PersonRolesTableTestFixture.cs" company="Starion Group S.A.">
-//    Copyright (c) 2023-2024 Starion Group S.A.
-//
-//    Authors: Sam Gerené, Alex Vorobiev, Alexander van Delft, Jaime Bernar, Antoine Théate, João Rua
-//
-//    This file is part of CDP4-COMET WEB Community Edition
-//    The CDP4-COMET WEB Community Edition is the Starion Web Application implementation of ECSS-E-TM-10-25 Annex A and Annex C.
-//
-//    The CDP4-COMET WEB Community Edition is free software; you can redistribute it and/or
-//    modify it under the terms of the GNU Affero General Public
-//    License as published by the Free Software Foundation; either
-//    version 3 of the License, or (at your option) any later version.
-//
-//    The CDP4-COMET WEB Community Edition is distributed in the hope that it will be useful,
-//    but WITHOUT ANY WARRANTY; without even the implied warranty of
-//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  <copyright file="PersonRolesTableTestFixture.cs" company="Starion Group S.A.">
+//     Copyright (c) 2024 Starion Group S.A.
+// 
+//     Authors: Sam Gerené, Alex Vorobiev, Alexander van Delft, Jaime Bernar, Théate Antoine, João Rua
+// 
+//     This file is part of COMET WEB Community Edition
+//     The COMET WEB Community Edition is the Starion Group Web Application implementation of ECSS-E-TM-10-25 Annex A and Annex C.
+// 
+//     The COMET WEB Community Edition is free software; you can redistribute it and/or
+//     modify it under the terms of the GNU Affero General Public
+//     License as published by the Free Software Foundation; either
+//     version 3 of the License, or (at your option) any later version.
+// 
+//     The COMET WEB Community Edition is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 //    Affero General Public License for more details.
-//
+// 
 //    You should have received a copy of the GNU Affero General Public License
 //    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// </copyright>
-// --------------------------------------------------------------------------------------------------------------------
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
 
 namespace COMETwebapp.Tests.Components.SiteDirectory.Roles
 {
-    using System.Linq;
-    using System.Threading.Tasks;
-
     using Bunit;
 
     using CDP4Common.SiteDirectoryData;
@@ -41,6 +38,7 @@ namespace COMETwebapp.Tests.Components.SiteDirectory.Roles
 
     using DynamicData;
 
+    using Microsoft.AspNetCore.Components.Forms;
     using Microsoft.Extensions.DependencyInjection;
 
     using Moq;
@@ -63,11 +61,11 @@ namespace COMETwebapp.Tests.Components.SiteDirectory.Roles
             this.context = new TestContext();
             this.viewModel = new Mock<IPersonRolesTableViewModel>();
 
-            this.personRole = new PersonRole()
+            this.personRole = new PersonRole
             {
                 Name = "Role 1",
                 ShortName = "role1",
-                Container = new SiteDirectory(){ ShortName = "siteDir" },
+                Container = new SiteDirectory { ShortName = "siteDir" }
             };
 
             var rows = new SourceList<PersonRoleRowViewModel>();
@@ -90,6 +88,37 @@ namespace COMETwebapp.Tests.Components.SiteDirectory.Roles
         }
 
         [Test]
+        public async Task VerifyAddingOrEditingPersonRole()
+        {
+            var addPersonRoleButton = this.renderer.FindComponents<DxButton>().First(x => x.Instance.Id == "dataItemDetailsButton");
+            await this.renderer.InvokeAsync(addPersonRoleButton.Instance.Click.InvokeAsync);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(this.renderer.Instance.ShouldCreateThing, Is.EqualTo(true));
+                Assert.That(this.viewModel.Object.CurrentThing, Is.InstanceOf(typeof(PersonRole)));
+            });
+
+            var personRolesGrid = this.renderer.FindComponent<DxGrid>();
+            await this.renderer.InvokeAsync(() => personRolesGrid.Instance.SelectedDataItemChanged.InvokeAsync(new PersonRoleRowViewModel(this.personRole)));
+            Assert.That(this.renderer.Instance.IsOnEditMode, Is.EqualTo(true));
+
+            var personRoleForm = this.renderer.FindComponent<PersonRoleForm>();
+            var personRoleEditForm = personRoleForm.FindComponent<EditForm>();
+            await personRoleForm.InvokeAsync(personRoleEditForm.Instance.OnValidSubmit.InvokeAsync);
+
+            Assert.Multiple(() =>
+            {
+                this.viewModel.Verify(x => x.CreateOrEditPersonRole(false), Times.Once);
+                Assert.That(this.viewModel.Object.CurrentThing, Is.InstanceOf(typeof(PersonRole)));
+            });
+
+            var form = this.renderer.FindComponent<DxGrid>();
+            await this.renderer.InvokeAsync(form.Instance.EditModelSaving.InvokeAsync);
+            this.viewModel.Verify(x => x.CreateOrEditPersonRole(false), Times.Once);
+        }
+
+        [Test]
         public void VerifyOnInitialized()
         {
             Assert.Multiple(() =>
@@ -99,38 +128,6 @@ namespace COMETwebapp.Tests.Components.SiteDirectory.Roles
                 Assert.That(this.renderer.Markup, Does.Contain(this.personRole.Name));
                 this.viewModel.Verify(x => x.InitializeViewModel(), Times.Once);
             });
-        }
-
-        [Test]
-        public async Task VerifyAddPersonRoleClick()
-        {
-            var addPersonRoleButton = this.renderer.FindComponents<DxButton>().First(x => x.Instance.Id == "addPersonRoleButton");
-            await this.renderer.InvokeAsync(addPersonRoleButton.Instance.Click.InvokeAsync);
-
-            var grid = this.renderer.FindComponent<DxGrid>();
-            Assert.That(grid.Instance.IsEditing(), Is.EqualTo(true));
-
-            await this.renderer.InvokeAsync(grid.Instance.EditModelSaving.InvokeAsync);
-            this.viewModel.Verify(x => x.CreateOrEditPersonRole(true), Times.Once);
-        }
-
-        [Test]
-        public async Task VerifyRowClick()
-        {
-            Assert.That(this.renderer.Instance.IsOnEditMode, Is.EqualTo(false));
-
-            var firstRow = this.viewModel.Object.Rows.Items.First();
-            var grid = this.renderer.FindComponent<DxGrid>();
-            await this.renderer.InvokeAsync(async () => await grid.Instance.SelectedDataItemChanged.InvokeAsync(firstRow));
-
-            Assert.That(this.renderer.Instance.IsOnEditMode, Is.EqualTo(true));
-
-            var details = this.renderer.FindComponent<PersonRoleDetails>();
-            await this.renderer.InvokeAsync(details.Instance.OnSubmit.InvokeAsync);
-            this.viewModel.Verify(x => x.CreateOrEditPersonRole(false), Times.Once);
-
-            await this.renderer.InvokeAsync(details.Instance.OnCancel.InvokeAsync);
-            Assert.That(this.renderer.Instance.IsOnEditMode, Is.EqualTo(false));
         }
     }
 }
