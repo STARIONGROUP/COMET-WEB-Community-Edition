a4a0858afffee3249c8d6cb1c812dfbac9bc75fc
Alexander van Delft <a.vandelft@rheagroup.com>
2025-01-07 13:27:50 +0100

    Add ElementBaseTreeRowViewModel tests

---
+++ COMETwebapp.Tests/ViewModels/Components/MultiModeleditor/ElementBaseTreeRowViewModelTestFixture.cs
@@ -0,0 +1,124 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="ElementBaseTreeRowViewModelTestFixture.cs" company="Starion Group S.A.">
+//     Copyright (c) 2024 Starion Group S.A.
+// 
+//     Authors: Sam Gerené, Alex Vorobiev, Alexander van Delft, Jaime Bernar, Théate Antoine, João Rua
+// 
+//     This file is part of COMET WEB Community Edition
+//     The COMET WEB Community Edition is the Starion Group Web Application implementation of ECSS-E-TM-10-25 Annex A and Annex C.
+// 
+//     The COMET WEB Community Edition is free software; you can redistribute it and/or
+//     modify it under the terms of the GNU Affero General Public
+//     License as published by the Free Software Foundation; either
+//     version 3 of the License, or (at your option) any later version.
+// 
+//     The COMET WEB Community Edition is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Affero General Public License for more details.
+// 
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace COMETwebapp.Tests.ViewModels.Components.ModelEditor
+{
+    using CDP4Common.EngineeringModelData;
+    using CDP4Common.SiteDirectoryData;
+
+    using COMETwebapp.ViewModels.Components.MultiModelEditor.Rows;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ElementBaseTreeRowViewModelTestFixture
+    {
+        private ElementDefinition elementBase;
+        private string elementName;
+        private DomainOfExpertise owner;
+        private string ownerShortName;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.elementName = "TestElement";
+            this.ownerShortName = "TestOwner";
+
+            this.owner = new DomainOfExpertise(Guid.NewGuid(), null, null)
+            {
+                ShortName = this.ownerShortName
+            };
+
+            this.elementBase = new ElementDefinition(Guid.NewGuid(), null, null)
+            {
+                Name = this.elementName,
+                Owner = this.owner
+            };
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void VerifyCreation()
+        {
+            var testVM = new ElementbaseTreeViewModelTest(this.elementBase);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(testVM.ElementBase, Is.EqualTo(this.elementBase));
+                Assert.That(testVM.ElementName, Is.EqualTo(this.elementName));
+                Assert.That(testVM.OwnerShortName, Is.EqualTo(this.ownerShortName));
+            });
+        }
+
+        [Test]
+        public void VerifyCreationNullElement()
+        {
+            Assert.That(() => new ElementbaseTreeViewModelTest(null), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void VerifyCreationAndUpdateProperties()
+        {
+            var testVM = new ElementbaseTreeViewModelTest();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(testVM.ElementBase, Is.Null);
+                Assert.That(testVM.ElementName, Is.Null);
+                Assert.That(testVM.OwnerShortName, Is.Null);
+            });
+
+            testVM.UpdateProperties(new ElementbaseTreeViewModelTest(this.elementBase));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(testVM.ElementBase, Is.EqualTo(this.elementBase));
+                Assert.That(testVM.ElementName, Is.EqualTo(this.elementName));
+                Assert.That(testVM.OwnerShortName, Is.EqualTo(this.ownerShortName));
+            });
+        }
+
+        [Test]
+        public void VerifyUpdatePropertiesNullElement()
+        {
+            Assert.That(() => new ElementbaseTreeViewModelTest(this.elementBase).UpdateProperties(null), Throws.ArgumentNullException);
+            Assert.That(() => new ElementbaseTreeViewModelTest().UpdateProperties(null), Throws.ArgumentNullException);
+        }
+    }
+
+    public class ElementbaseTreeViewModelTest : ElementBaseTreeRowViewModel
+    {
+        public ElementbaseTreeViewModelTest(ElementBase elementBase) : base(elementBase)
+        {
+        }
+
+        public ElementbaseTreeViewModelTest()
+        {
+        }
+    }
+}
