0f617afffa2563f7c69ad0d54146fc200688ccf8
João Rua <140734849+joao4all@users.noreply.github.com>
2026-07-28 12:02:19 +0100

    Fix #800 [Bug] No validation on the ShortName of element definitions

---
+++ COMETwebapp.Tests/Validators/ModelEditor/ElementDefinitionValidatorTestFixture.cs
@@ -0,0 +1,64 @@
+// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="ElementDefinitionValidatorTestFixture.cs" company="Starion Group S.A.">
+//     Copyright (c) 2023-2026 Starion Group S.A.
+//
+//     This file is part of COMET WEB Community Edition
+//     The COMET WEB Community Edition is the Starion Group Web Application implementation of ECSS-E-TM-10-25 Annex A and Annex C.
+// 
+//     The COMET WEB Community Edition is free software; you can redistribute it and/or
+//     modify it under the terms of the GNU Affero General Public
+//     License as published by the Free Software Foundation; either
+//     version 3 of the License, or (at your option) any later version.
+// 
+//     The COMET WEB Community Edition is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Affero General Public License for more details.
+// 
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace COMETwebapp.Tests.Validators.ModelEditor
+{
+    using CDP4Common.EngineeringModelData;
+    using CDP4Common.SiteDirectoryData;
+    using CDP4Common.Validation;
+
+    using COMETwebapp.Validators.ModelEditor;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ElementDefinitionValidatorTestFixture
+    {
+        private ElementDefinitionValidator validator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var validationService = new ValidationService();
+            this.validator = new ElementDefinitionValidator(validationService);
+        }
+
+        [Test]
+        public void VerifyValidationScenarios()
+        {
+            var elementDefinition = new ElementDefinition();
+            Assert.That(this.validator.Validate(elementDefinition).IsValid, Is.EqualTo(false));
+
+            elementDefinition = new ElementDefinition
+            {
+                Name = "element1",
+                ShortName = "element short name",
+                Owner = new DomainOfExpertise()
+            };
+
+            Assert.That(this.validator.Validate(elementDefinition).IsValid, Is.EqualTo(false));
+
+            elementDefinition.ShortName = "elementShortName";
+            Assert.That(this.validator.Validate(elementDefinition).IsValid, Is.EqualTo(true));
+        }
+    }
+}
---
+++ COMETwebapp.Tests/Validators/ReferenceData/MeasurementScales/MappingToReferenceScaleValidatorTestFixture.cs
@@ -0,0 +1,61 @@
+// --------------------------------------------------------------------------------------------------------------------
+//  <copyright file="MappingToReferenceScaleValidatorTestFixture.cs" company="Starion Group S.A.">
+//     Copyright (c) 2023-2026 Starion Group S.A.
+//
+//     This file is part of COMET WEB Community Edition
+//     The COMET WEB Community Edition is the Starion Group Web Application implementation of ECSS-E-TM-10-25 Annex A and Annex C.
+// 
+//     The COMET WEB Community Edition is free software; you can redistribute it and/or
+//     modify it under the terms of the GNU Affero General Public
+//     License as published by the Free Software Foundation; either
+//     version 3 of the License, or (at your option) any later version.
+// 
+//     The COMET WEB Community Edition is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Affero General Public License for more details.
+// 
+//    You should have received a copy of the GNU Affero General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  </copyright>
+//  --------------------------------------------------------------------------------------------------------------------
+
+namespace COMETwebapp.Tests.Validators.ReferenceData.MeasurementScales
+{
+    using CDP4Common.SiteDirectoryData;
+    using CDP4Common.Validation;
+
+    using COMETwebapp.Validators.ReferenceData.MeasurementScales;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class MappingToReferenceScaleValidatorTestFixture
+    {
+        private MappingToReferenceScaleValidator validator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var validationService = new ValidationService();
+            this.validator = new MappingToReferenceScaleValidator(validationService);
+        }
+
+        [Test]
+        public void VerifyValidationScenarios()
+        {
+            var mapping = new MappingToReferenceScale();
+            Assert.That(this.validator.Validate(mapping).IsValid, Is.EqualTo(false));
+
+            mapping = new MappingToReferenceScale
+            {
+                ReferenceScaleValue = new ScaleValueDefinition()
+            };
+
+            Assert.That(this.validator.Validate(mapping).IsValid, Is.EqualTo(false));
+
+            mapping.DependentScaleValue = new ScaleValueDefinition();
+            Assert.That(this.validator.Validate(mapping).IsValid, Is.EqualTo(true));
+        }
+    }
+}
