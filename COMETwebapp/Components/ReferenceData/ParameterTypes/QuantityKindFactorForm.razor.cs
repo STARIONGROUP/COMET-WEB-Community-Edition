53fcbd3fedbc8bc757154107486fe57cb0732da8
jp.costarua <jp.costarua@ext.stariongroup.eu>
2026-07-31 12:22:37 +0100

    quantity kind

--- COMETwebapp/Components/ReferenceData/ParameterTypes/QuantityKindFactorForm.razor.cs
+++ COMETwebapp/Components/ReferenceData/ParameterTypes/QuantityKindFactorForm.razor.cs
@@ -1,5 +1,5 @@
 // --------------------------------------------------------------------------------------------------------------------
-//  <copyright file="QuantityKindFactorsTable.razor.cs" company="Starion Group S.A.">
+//  <copyright file="QuantityKindFactorForm.razor.cs" company="Starion Group S.A.">
 //     Copyright (c) 2023-2026 Starion Group S.A.
 //
 //     This file is part of COMET WEB Community Edition
@@ -23,27 +23,26 @@
 namespace COMETwebapp.Components.ReferenceData.ParameterTypes
 {
     using CDP4Common.SiteDirectoryData;
-    using CDP4Common.Types;
 
     using COMETwebapp.Components.Common;
-    using COMETwebapp.ViewModels.Components.ReferenceData.Rows;
 
     using Microsoft.AspNetCore.Components;
 
     /// <summary>
-    /// Support class for the <see cref="QuantityKindFactorsTable" />
+    /// Support class for the <see cref="QuantityKindFactorForm" /> component.
     /// </summary>
-    public partial class QuantityKindFactorsTable : ThingOrderedItemsTable<DerivedQuantityKind, QuantityKindFactor, QuantityKindFactorRowViewModel>
+    public partial class QuantityKindFactorForm : SelectedDataItemForm
     {
         /// <summary>
-        /// Gets or sets the collection of <see cref="ParameterType" />s of <see cref="QuantityKind" />
+        /// Gets or sets the <see cref="QuantityKindFactor" /> item being created or edited.
         /// </summary>
         [Parameter]
-        public IEnumerable<QuantityKind> QuantityKindParameterTypes { get; set; }
+        public QuantityKindFactor Item { get; set; }
 
         /// <summary>
-        /// Gets or sets the ordered list of items from the current <see cref="ThingOrderedItemsTable{T,TItem,TItemRow}.Thing"/>
+        /// Gets or sets the collection of available <see cref="QuantityKind" /> parameter types.
         /// </summary>
-        public override OrderedItemList<QuantityKindFactor> OrderedItemsList => this.Thing.QuantityKindFactor;
+        [Parameter]
+        public IEnumerable<QuantityKind> QuantityKindParameterTypes { get; set; } = [];
     }
 }
