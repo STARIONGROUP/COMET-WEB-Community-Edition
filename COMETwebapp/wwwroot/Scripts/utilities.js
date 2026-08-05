71ce5e712993a2d89a7793d72d4aa83b5fd7e8e3
João Rua <140734849+joao4all@users.noreply.github.com>
2026-08-05 08:25:01 +0100

    Fix #891 [Bug]: Ten megabytes before you've logged in (#902)

--- COMETwebapp/wwwroot/Scripts/utilities.js
+++ COMETwebapp/wwwroot/Scripts/utilities.js
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * Downloads a given file by its name
  * @param {string} fileName
  * @param {any} contentStreamReference
@@ -30,4 +30,45 @@ function GetItemDimensions(cssSelector) {
         return [element.clientWidth, element.clientHeight];
     }
     return [];
+}
+
+let babylonScriptsPromise = null;
+
+/**
+ * Dynamically loads Babylon.js scripts on demand
+ * @returns {Promise<void>}
+ */
+function loadBabylonScripts() {
+    if (babylonScriptsPromise) {
+        return babylonScriptsPromise;
+    }
+
+    const scripts = [
+        "Scripts/BabylonJS/babylon.js",
+        "Scripts/BabylonJS/babylonjs.loaders.min.js",
+        "Scripts/SceneObject.js",
+        "Scripts/MeshMaterial.js",
+        "Scripts/babylonSpecifics.js",
+        "Scripts/babylonInterop.js"
+    ];
+
+    babylonScriptsPromise = scripts.reduce((promise, src) => {
+        return promise.then(() => new Promise((resolve, reject) => {
+            if (document.querySelector(`script[src="${src}"]`)) {
+                resolve();
+                return;
+            }
+            const script = document.createElement("script");
+            script.src = src;
+            script.onload = resolve;
+            script.onerror = () => {
+                script.remove();
+                babylonScriptsPromise = null;
+                reject(new Error(`Failed to load script: ${src}`));
+            };
+            document.body.appendChild(script);
+        }));
+    }, Promise.resolve());
+
+    return babylonScriptsPromise;
 }
